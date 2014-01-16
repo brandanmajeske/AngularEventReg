bc069c3782e011784d37fadde66267759aa12ef0
Brandan <bmajeske@me.com>
2014-01-15 21:48:25 -0700

    event.date
    playing with is future validation for kicks

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,6 +2,8 @@
 
 eventsApp.controller('EditEventController',
 	function EditEventController($scope){
+
+
 		$scope.saveEvent = function(event, newEventForm){
 			console.log(newEventForm);
 			if(newEventForm.$valid) {
@@ -11,6 +13,52 @@ eventsApp.controller('EditEventController',
 
 		$scope.cancelEdit = function(){
 			window.location = "/eventdetails.html";
-		};	
-	}
+		};
+
+		$scope.date = Math.round(new Date().getTime());
+
+		$scope.validDate = false;
+
+		$scope.event = {
+			name: 'New Event',
+			date: $scope.date,
+			location: {
+				address: '',
+				city: '',
+				state: ''
+			},
+			price: 0,
+			imageUrl: ''
+
+		};
+
+		$scope.isFuture = function (date) {
+			
+			var today = Math.round(new Date().getTime());
+			var newDate = new Date(date).getTime();
+			if(newDate > today) {
+				return true;
+			} else {
+				//event.date.$setValidity('invalid', true);
+				return false;
+			}
+
+		};		
+
+		/*$scope.isFuture = function (date) {
+			
+			var today = Math.round(new Date().getTime());
+			var newDate = new Date(date).getTime();
+			if(newDate > today) {
+				$scope.validDate = true;
+				return $scope.validDate;
+			} else {
+				$scope.validDate = false;
+			}
+			$scope.date = newDate;
+			return $scope.date;
+		};*/
+	
+	} // end EditEventController
 );
+
