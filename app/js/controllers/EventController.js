4ce22391c331943c300619e6d1d115b064fb13c7
Brandan <bmajeske@me.com>
2014-01-20 15:34:35 -0700

    Added cookieStore to upVote function on event controller

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-eventsApp.controller('EventController', function EventController($scope, eventData, $routeParams, $route  ){
+eventsApp.controller('EventController', function EventController($scope, eventData, $routeParams, $route, $cookieStore  ){
 
 	$scope.alternate = 'alternate';
     $scope.sortorder = 'name';
@@ -11,12 +11,40 @@ eventsApp.controller('EventController', function EventController($scope, eventDa
             $scope.event = event;
         });*/
 
-	$scope.upVoteSession = function(session, event) {
-		session.upVoteCount++;
-	}
+    //$cookieStore.remove('upVote');
+    console.log($cookieStore.get('upVote'));
 
-	$scope.downVoteSession = function(session){
-		session.upVoteCount--;
-	}
+
+        $scope.upVoteSession = function(session, event) {
+
+            if(typeof $cookieStore.get('upVote') === 'undefined'){
+
+            session.upVoteCount++;
+            console.log(session.upVoteCount);
+            $cookieStore.put('upVote', session.upVoteCount);
+            console.log($cookieStore.get('upVote'));
+
+            } else {
+                window.alert('Sorry! You\'ve already voted.');
+            }
+        }
+
+        $scope.downVoteSession = function(session, event){
+
+
+
+            if(typeof $cookieStore.get('downVote') === 'undefined'){
+                session.upVoteCount--;
+                console.log(event);
+                $cookieStore.put('downVote', session.upVoteCount);
+                console.log($cookieStore.get('downVote'));
+
+            } else {
+                window.alert('Sorry! You\'ve already voted.');
+            }
+        }
+
+
+
 
 });
